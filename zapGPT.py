54f86a6e20a4de901218757cac1e3937751e2634
Vanderson Martins do Rosario <vandersonmr2@gmail.com>
2023-04-08 18:36:22 -0700

    Init
    Signed-off-by: Vanderson Martins do Rosario <vandersonmr2@gmail.com>

---
+++ zapGPT.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import openai
+import chromadb
+import OAIWrapper
+import random
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+oai = OAIWrapper.OAIWrapper()
+
+def load_zap_chat_log(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+    filtered_lines = []
+    for line in lines:
+        if "<Media omitted>" not in line:
+            try:
+                timestamp, content = line.strip().split(" - ", 1)
+                user_id, message = content.split(": ", 1)
+                filtered_lines.append((user_id, message))
+            finally:
+                continue
+
+    return filtered_lines
+
+def gen_proompt_to_imitate(user_conversation, max_size):
+    proompt = """Describe in details the away that a user chatts based on the examples I will give.
+                 You should focus on the language used, and the write style, not the content.
+                 You anwser should include examples of the user chatting patterns and the language that the user uses.
+                 Pick some of the parts of the message to give as example. Follows messages from this user: """
+    examples = [user_id+': '+msg for user_id, msg in user_conversation[0:max_size]]
+    proompt = proompt + '\n'.join(examples)
+    proompt_to_imitate = oai.chat_completion(proompt)
+    return proompt_to_imitate
+
+def gen_next_msgs(user_id, user_description, context):
+    proompt_name = "You are going to imitate an user named: " + user_id
+    proompt_description = "This user texts looks like as follow: " + user_description
+    proompt_context = "These were the last messsages on the conversation: "+ "\n".join([user+': '+msg for user, msg in context])
+    proompt_question = """Give me five possible messages that could be used to continue the conversation imitating that user.
+                          Your anwser should only include the messages.
+                          Each message should be a possible follow up of the last messages in the conversation.
+                          Each message should have just one line. Do not enumerate them."""
+    proompt = proompt_name + '\n' + proompt_description + '\n' + proompt_context + '\n' + proompt_question
+    msgs = oai.chat_completion(proompt)
+    return msgs.strip(user_id+':').split('\n')
+
+conversation = load_zap_chat_log(sys.argv[1])
+user_to_imitate = sys.argv[2]
+user_conversation = [(user_id, message) for user_id, message in conversation if  user_to_imitate in user_id]
+
+
+user_description = gen_proompt_to_imitate(user_conversation, 31)
+
+while(True):
+    context = conversation[-5:]
+    possible_msgs = gen_next_msgs(user_to_imitate, user_description, context)
+    for i, msg in enumerate(possible_msgs):
+        print("({}) {}".format(i, msg))
+
+    print("Which msg (msg id) do you choose: ")
+    msg_id = int(input())
+    next_msg = possible_msgs[msg_id]
+    conversation.append((user_to_imitate, next_msg))
+
+    print(user_to_imitate + ': ' + next_msg)
+    print("Input the user for the message in the conversation:")
+    awnser_user = input()
+    print("Input the next message in the conversation:")
+    awnser_msg = input()
+    conversation.append((awnser_user, awnser_msg))
+
+
+
